690751ba7cfda7fc887f91013798c278694282ce
Sam Hankley <shankley@ad.unc.edu>
2022-01-29 10:44:46 -0500

    progress on ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,10 +4,33 @@ __author__ = "730435749"
 
 secret: str = "python"
 guess: str = input("What is your 6-letter guess? ")
+result: str = ""
+i: int = 0
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
 
 while len(guess) != 6:
     guess = input("That was not 6 letters! Try again: ")
 
+while i < len(secret):
+    if guess[i] == secret[i]:
+        result = result + GREEN_BOX
+    else:
+        guess_check: bool = False
+        alt_secret_index: int = 0
+        while alt_secret_index < len(secret) and not guess_check:
+            if secret[alt_secret_index] == guess[i]:
+                guess_check = True
+            else:
+                alt_secret_index = alt_secret_index + 1
+        if guess_check:
+            result = result + YELLOW_BOX
+        else:
+            result = result + WHITE_BOX
+    i = i + 1
+
+print(result)
 
 if guess != secret:
     print("Not quite. Play again soon!")
