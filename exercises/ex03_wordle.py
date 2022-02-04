3ad612368b0042cdbc3ef324f8cb0a0c13eee9e0
Sam Hankley <shankley@ad.unc.edu>
2022-02-04 00:35:02 -0500

    almost done with ex03

---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,43 @@
+"""Totally original wordle game."""
+
+__author__ = 730435749
+
+
+def contains_char(string_searched: str, character_searched: str) -> bool:
+    """Searches for a single character within any given string."""
+    assert len(character_searched) == 1
+    i: int = 0
+    while i < len(string_searched):
+        if string_searched[i] == character_searched:
+            return True
+        else:
+            i += 1
+    return False
+
+
+def emojified(guess: str, secret: str) -> str:
+    """Searches all characters within two strings and uses emojis to relay the relative indices of each character searched."""
+    assert len(guess) == len(secret)
+    WHITE_BOX: str = "\U00002B1C"
+    GREEN_BOX: str = "\U0001F7E9"
+    YELLOW_BOX: str = "\U0001F7E8"
+    result: str = ""
+    counter: int = 0
+    while counter < len(secret):
+        if contains_char(secret, guess[counter]) is True:
+            if guess[counter] == secret[counter]:
+                result += GREEN_BOX
+            else:
+                result += YELLOW_BOX
+        else:
+            result += WHITE_BOX
+        counter += 1
+    return result
+
+
+def input_guess(expected_guess_length: int) -> str:
+    """Ensures the user guesses a string of proper length."""
+    guess: str = input(f"Enter a {expected_guess_length} character word: ")
+    while len(guess) != expected_guess_length:
+        guess = input(f"That wasn't {expected_guess_length} chars! Try again: ")
+    return guess
